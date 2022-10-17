96c7e41def4416ba663a6ebcf80d7232aeabb501
tan <tanmaykm@gmail.com>
2022-10-17 16:09:31 +0530

    HTTP 1.x compat
    Update HTTPDownloads to be compatible with HTTP 1.x.

--- src/HTTPDownloads.jl
+++ src/HTTPDownloads.jl
@@ -16,8 +16,6 @@ import NetworkOptions
 using URIs
 using HTTP
 
-abstract type LibCurlLayer{Next <: HTTP.Layer} <: HTTP.Layer{Next} end
-
 const DOWNLOAD_LOCK = ReentrantLock()
 const DOWNLOADER = Ref{Union{Nothing,Downloader}}(nothing)
 
@@ -42,17 +40,20 @@ function get_downloader()
     end
 end
 
+function is_custom_sslconfig(sslconfig)
+    isnothing(sslconfig) && return false
+    (sslconfig === HTTP.ConnectionPool.nosslconfig) && return false
+    return true
+end
 
-function HTTP.request(::Type{LibCurlLayer{Next}}, url::URI, req, body;
-            response_stream=nothing,
+function downloads_layer(handler)
+    return function downloads_handler(req::HTTP.Request;
             iofunction=nothing,
 
             # # TimeoutLayer
             readtimeout=0,
 
             # # ConnectionLayer
-            socket_type::Type=TCPSocket,
-
             verbose::Int=0,
 
             # List of all HTTP.jl keywords for the connection pool and stream
@@ -72,157 +73,173 @@ function HTTP.request(::Type{LibCurlLayer{Next}}, url::URI, req, body;
             #     # Similar to the Downloader's grace parameter
 
             # ConnectionPool / sslconnection
-            require_ssl_verification::Bool=NetworkOptions.verify_host(url.host, "SSL"),
-            sslconfig=nothing,
+            require_ssl_verification::Bool=NetworkOptions.verify_host(req.url.host, "SSL"),
+            sslconfig=HTTP.ConnectionPool.nosslconfig,
 
             keepalive::Bool=false,
             connect_timeout::Int=0,
 
             proxy::Union{AbstractString,Nothing}=nothing,
-            # * reuse_limit =
-            #     No equiv? Ignored for now
 
             # # StreamLayer
-            # * reached_redirect_limit=false,
+            # * redirect_limit=false,
             #     FIXME ???
             # * iofunction=nothing,
             #     TODO: Figure out callbacks?
             # * verbose::Int=0,
+            decompress=nothing,
+            kw...
+        )
 
-            kw...) where Next
+        response_stream = HTTP.isbytes(req.response.body) ? nothing : req.response.body
 
-    if iofunction !== nothing    || socket_type !== TCPSocket || sslconfig !== nothing
-        # Fallback to pure-Julia implementation in HTTP.ConnectionPool for
-        # options we can't handle with libcurl.
-        #
-        # FIXME: This is required until we can figure out how to expose this
-        # functionality via libcurl
-        if sslconfig === nothing
-            sslconfig = HTTP.ConnectionPool.nosslconfig
+        if iofunction !== nothing || is_custom_sslconfig(sslconfig)
+            # Fallback to pure-Julia implementation in HTTP.jl for
+            # options we can't handle with libcurl.
+            return handler(req;
+                # response_stream=response_stream,
+                iofunction=iofunction,
+                readtimeout=readtimeout,
+                verbose=verbose,
+                require_ssl_verification=require_ssl_verification,
+                sslconfig=sslconfig,
+                keepalive=keepalive,
+                connect_timeout=connect_timeout,
+                proxy=proxy,
+                kw...
+            )
         end
-        return HTTP.request(Next, url, req, body;
-                            response_stream=response_stream,
-                            iofunction=iofunction,
-                            readtimeout=readtimeout,
-                            socket_type=socket_type,
-                            sslconfig=sslconfig,
-                            kw...)
-    end
 
-    input = req.body === HTTP.body_is_a_stream ? body :
-            length(req.body) > 0 ? IOBuffer(req.body) : nothing
-
-    output_buf = nothing
-    output = response_stream
-    if response_stream === nothing
-        # HTTP.jl assumes you always want the body by default.
-        output = output_buf = IOBuffer()
-    end
-
-    function per_request_easy_hook(easy)
-        if !require_ssl_verification
-            Curl.setopt(easy, Curl.CURLOPT_SSL_VERIFYPEER, false)
-        end
-        if keepalive
-            Curl.setopt(easy, Curl.CURLOPT_TCP_KEEPALIVE, true)
-        end
-        if proxy !== nothing
-            Curl.setopt(easy, Curl.CURLOPT_PROXY, proxy)
-        end
-        if connect_timeout > 0
-            Curl.setopt(easy, Curl.CURLOPT_CONNECTTIMEOUT, connect_timeout)
-            # See also https://curl.se/libcurl/c/CURLOPT_CONNECTTIMEOUT_MS.html
-            # if we want to allow subsecond timeouts.
-        end
-    end
-
-    response = Downloads.request(string(url);
-        downloader = get_downloader(),
-        method = req.method,
-        headers = req.headers,
-        input = input,
-        output = output,
-        timeout = readtimeout <= 0 ? Inf : readtimeout,
-        throw = false,
-        verbose = verbose > 0,
-        easy_hook = per_request_easy_hook)
-
-    if response isa Downloads.RequestError
-        # We couldn't even get a response. There's a huge list of possible
-        # return codes in libcurl's curl.h.
-        #
-        # Users of HTTP.jl largely seem to match these based on whether they're
-        # "retryable", and expect to see HTTP.jl's error types. This informal
-        # API is hard to match! For now:
-        #
-        #   * If an error "seems retryable", wrap in HTTP.IOError
-        #   * For other cases, just throw the RequestError
-        #   * Ugh.
-        #
-        #
-        # As an example, AWS.jl has the following logic:
-        #
-        #  # Base.IOError is needed because HTTP.jl can often have errors that aren't
-        #  # caught and wrapped in an HTTP.IOError
-        #  # https://github.com/JuliaWeb/HTTP.jl/issues/382
-        #  @delay_retry if isa(e, Sockets.DNSError) ||
-        #                  isa(e, HTTP.ParseError) ||
-        #                  isa(e, HTTP.IOError) ||
-        #                  isa(e, Base.IOError) ||
-        #                  (isa(e, HTTP.StatusError) && _http_status(e) >= 500)
-        #
-        if response.code == Curl.CURLE_OPERATION_TIMEDOUT
-            # HTTP throws
-            #   * HTTP.TimeoutRequest.ReadTimeoutError for readtimeout
-            #   * HTTP.ConnectionPool.ConnectTimeout   for connect_timeout
-            # CURL doesn't distinguish between these however, so it's not easy
-            # to know which one we've hit here.
-            t = readtimeout > 0 ? readtimeout : connect_timeout
-            throw(HTTP.TimeoutRequest.ReadTimeoutError(t))
-            # TODO: Would it be better just to throw(response) ??
-        elseif response.code in (Curl.CURLE_PEER_FAILED_VERIFICATION,
-                                 Curl.CURLE_SSL_CERTPROBLEM,
-                                 Curl.CURLE_SSL_CIPHER,
-                                 Curl.CURLE_SSL_CACERT_BADFILE,
-                                 Curl.CURLE_SSL_CRL_BADFILE,
-                                 Curl.CURLE_SSL_ISSUER_ERROR)
-            # SSL certificate problems - not retryable, so just throw the
-            # original error.
-            throw(response)
+        input = nothing
+        if HTTP.isbytes(req.body)
+            input_bytes = HTTP.bytes(req.body)
+            if length(input_bytes) > 0
+                input = IOBuffer(input_bytes)
+            end
         else
-            # Wrap other errors in HTTP.IOError. Downstream codes will likely
-            # assume all such errors are retry-able
-            #
-            throw(HTTP.IOError(response, response.message))
+            input = req.body
         end
+
+        if response_stream === nothing
+            # HTTP.jl assumes you always want the body by default.
+            output = IOBuffer()
+        else
+            output = response_stream
+            truncate(output, 0)
+        end
+
+        function per_request_easy_hook(easy)
+            if !require_ssl_verification
+                Curl.setopt(easy, Curl.CURLOPT_SSL_VERIFYPEER, false)
+            end
+            if keepalive
+                Curl.setopt(easy, Curl.CURLOPT_TCP_KEEPALIVE, true)
+            end
+            if proxy !== nothing
+                Curl.setopt(easy, Curl.CURLOPT_PROXY, proxy)
+            end
+            if connect_timeout > 0
+                Curl.setopt(easy, Curl.CURLOPT_CONNECTTIMEOUT, connect_timeout)
+                # See also https://curl.se/libcurl/c/CURLOPT_CONNECTTIMEOUT_MS.html
+                # if we want to allow subsecond timeouts.
+            end
+        end
+
+        response = Downloads.request(string(req.url);
+            downloader = get_downloader(),
+            method = req.method,
+            headers = req.headers,
+            input = input,
+            output = output,
+            timeout = readtimeout <= 0 ? Inf : readtimeout,
+            throw = false,
+            verbose = verbose > 0,
+            easy_hook = per_request_easy_hook,
+            decompress = decompress,
+        )
+
+        if response isa Downloads.RequestError
+            # We couldn't even get a response. There's a huge list of possible
+            # return codes in libcurl's curl.h.
+            #
+            # Users of HTTP.jl largely seem to match these based on whether they're
+            # "retryable", and expect to see HTTP.jl's error types. This informal
+            # API is hard to match! For now:
+            #
+            #   * If an error "seems retryable", wrap in HTTP.IOError
+            #   * For other cases, just throw the RequestError
+            #   * Ugh.
+            #
+            #
+            # As an example, AWS.jl has the following logic:
+            #
+            #  # Base.IOError is needed because HTTP.jl can often have errors that aren't
+            #  # caught and wrapped in an HTTP.IOError
+            #  # https://github.com/JuliaWeb/HTTP.jl/issues/382
+            #  @delay_retry if isa(e, Sockets.DNSError) ||
+            #                  isa(e, HTTP.ParseError) ||
+            #                  isa(e, HTTP.IOError) ||
+            #                  isa(e, Base.IOError) ||
+            #                  (isa(e, HTTP.StatusError) && _http_status(e) >= 500)
+            #
+            if response.code == Curl.CURLE_OPERATION_TIMEDOUT
+                # HTTP throws
+                #   * HTTP.TimeoutError for readtimeout
+                #   * HTTP.ConnectionPool.ConnectTimeout   for connect_timeout
+                # CURL doesn't distinguish between these however, so it's not easy
+                # to know which one we've hit here.
+                t = readtimeout > 0 ? readtimeout : connect_timeout
+                throw(HTTP.TimeoutError(t))
+                # TODO: Would it be better just to throw(response) ??
+            elseif response.code in (Curl.CURLE_PEER_FAILED_VERIFICATION,
+                                    Curl.CURLE_SSL_CERTPROBLEM,
+                                    Curl.CURLE_SSL_CIPHER,
+                                    Curl.CURLE_SSL_CACERT_BADFILE,
+                                    Curl.CURLE_SSL_CRL_BADFILE,
+                                    Curl.CURLE_SSL_ISSUER_ERROR)
+                # SSL certificate problems - not retryable, so just throw the
+                # original error.
+                throw(response)
+            else
+                # Wrap other errors in HTTP.IOError. Downstream codes will likely
+                # assume all such errors are retry-able
+                #
+                throw(HTTP.IOError(response, response.message))
+            end
+        end
+
+        # Convert output back into HTTP.Response
+        body = (response_stream === nothing) ? take!(output) : output
+
+        res = HTTP.Response(response.status, HTTP.mkheaders(response.headers);
+                            body=body, request=req)
+
+        return res
     end
-
-    # Convert output back into HTTP.Response
-    body = output_buf === nothing ?
-           HTTP.MessageRequest.body_was_streamed :
-           take!(output_buf)
-
-    res = HTTP.Response(response.status, HTTP.mkheaders(response.headers);
-                        body=body, request=req)
 end
 
 function has_http_layer(stack, layer)
-    while true
-        if stack === Union{}
-            return false
-        elseif stack <: layer
+    for stacked_layer in stack
+        if layer === stacked_layer
             return true
         end
-        stack = stack.parameters[1]
     end
+    return false
 end
 
 function set_downloads_backend(use_downloads::Bool=true)
-    stack = HTTP.stack()
-    if use_downloads && !has_http_layer(stack, LibCurlLayer)
-        HTTP.insert_default!(ConnectionPoolLayer, LibCurlLayer)
-    elseif !use_downloads && has_http_layer(stack, LibCurlLayer)
-        HTTP.remove_default!(ConnectionPoolLayer, LibCurlLayer)
+    if use_downloads
+        # since we are taking over from the connection layer, we need to add back the exceptionlayer here
+        if !has_http_layer(HTTP.REQUEST_LAYERS, HTTP.exceptionlayer)
+            HTTP.pushlayer!(HTTP.exceptionlayer)
+        end
+        if !has_http_layer(HTTP.REQUEST_LAYERS, downloads_layer)
+            HTTP.pushlayer!(downloads_layer)
+        end
+    else
+        filter!(HTTP.REQUEST_LAYERS) do layer
+            layer !== downloads_layer && layer !== HTTP.exceptionlayer
+        end
     end
     nothing
 end
